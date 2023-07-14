a920b6e91694c5c363f4e4daefeaa426a9e70e8c
Kendall <kendallawills@gmail.com>
2023-07-14 17:34:31 -0400

    Determine Winner Function

---
+++ RockPaperScissor.js
@@ -0,0 +1,57 @@
+console.log('hi');
+
+const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+        return userInput;
+    } else {
+        console.log('Error!');
+    }
+}
+
+console.log('The user chose: ' + getUserChoice('Rock'));
+
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+console.log('The computer chose: ' + getComputerChoice());
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return 'The game is a tie!';
+    }
+    if (userChoice === 'rock') {
+        if (computerChoice === 'paper') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+
+    if (userChoice === 'paper') {
+        if (computerChoice === 'scissors') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+
+    if (userChoice === 'scissors') {
+        if (computerChoice === 'rock') {
+            return 'The computer won!';
+        } else {
+            return 'You won!';
+        }
+    }
+}
+
+console.log(determineWinner('paper', 'scissors'));
